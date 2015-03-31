a5782c5989cc51128b0bbd3faa9e2be06078f56a
Thales Paiva <thalespaiva@gmail.com>
2015-03-31 04:24:36 -0300

    Passing correct info to the schedule view.

--- koumparos/teacher/views.py
+++ koumparos/teacher/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 from django.shortcuts import render_to_response
 
-from teacher.models import Teacher, Class
+from teacher.models import Teacher, Student, Class
 
 # Create your views here.
 
@@ -16,6 +16,8 @@ def overview(request, id):
 
 def schedule(request, id):
     info = {
-        'classes': Class.objects.all()
+        'teacher': Teacher.objects.filter(id=id)[0],
+        'classes': Class.objects.filter(teacher=id),
+        'students': Student.objects.all(),
     }
-    return render_to_response('teacher/overview.html', info)
+    return render_to_response('teacher/schedule.html', info)
