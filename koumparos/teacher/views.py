5b6a619be33263d68355f6b416d71bf34abcdf75
Thales Paiva <thalespaiva@gmail.com>
2015-03-31 01:45:42 -0300

    Separing overview and schedule pages.

--- koumparos/teacher/views.py
+++ koumparos/teacher/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.shortcuts import render_to_response
 
-from teacher.models import Class
+from teacher.models import Teacher, Class
 
 # Create your views here.
-def home(request):
-    return render_to_response('teacher/overview.html', {'classes' : Class.objects.all()})
+
+
+def overview(request, id):
+    info = {
+        'teacher': Teacher.objects.filter(id=id)[0],
+        'classes': Class.objects.filter(teacher=id),
+    }
+    return render_to_response('teacher/overview.html', info)
+
+
+def schedule(request, id):
+    info = {
+        'classes': Class.objects.all()
+    }
+    return render_to_response('teacher/overview.html', info)
