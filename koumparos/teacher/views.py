5fc4361bc711d1360cec2bdbf57d8f06742c89c7
Thales Paiva <thalespaiva@gmail.com>
2015-03-31 00:58:38 -0300

    Passing classes infos to template.

--- koumparos/teacher/views.py
+++ koumparos/teacher/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 
+from teacher.models import Class
 
 # Create your views here.
 def home(request):
-    return render_to_response('teacher/overview.html')
+    return render_to_response('teacher/overview.html', {'classes' : Class.objects.all()})
