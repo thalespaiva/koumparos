218c2bd79d7a34bc33bb0ba101b7c4ad3d22e7b9
Thales Paiva <thalespaiva@gmail.com>
2015-03-31 00:57:27 -0300

    Adding the models to admin page.

--- koumparos/teacher/admin.py
+++ koumparos/teacher/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from teacher.models import Teacher, Student, Class
+
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Class)
