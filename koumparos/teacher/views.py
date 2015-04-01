07bccf14c65105bf8979a35ee2cef17bf33419eb
Thales Paiva <thalespaiva@gmail.com>
2015-04-01 01:37:23 -0300

    Can schedule classes for any number of weeks.

--- koumparos/teacher/views.py
+++ koumparos/teacher/views.py
@@ -1,7 +1,9 @@
 # from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+from datetime import datetime, timedelta
 
 from teacher.models import Teacher, Student, Class
 
@@ -27,16 +29,20 @@ def schedule(request, id):
         student = Student.objects.filter(id=request.POST['student_id'])[0]
         teacher = info['teacher']
         price = request.POST['price']
-        date = request.POST['class_date']
+        base_date = request.POST['class_date']
         start = request.POST['start_time']
         finish = request.POST['finish_time']
         location = request.POST['location']
 
-        new_class = Class(
-            student=student, teacher=teacher, subject='MATH',
-            price=price, location=location, date=date, start_time=start,
-            finish_time=finish)
-        new_class.save()
+        number_of_weeks = request.POST['number_of_weeks']
+        date = datetime.strptime(base_date, '%Y-%m-%d')
+        for i in range(int(number_of_weeks)):
+            new_class = Class(
+                student=student, teacher=teacher, subject='MATH',
+                price=price, location=location, date=date.date(),
+                start_time=start, finish_time=finish)
+            new_class.save()
+            date = date + timedelta(days=7)
 
     return render_to_response('teacher/schedule.html', info,
                               RequestContext(request))
